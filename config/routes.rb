d463f701d2dfb0b2f3ecdd718903b88708e2b4f8
Mnelson1992 <nelson.matthew2015@gmail.com>
2018-02-12 22:41:19 -0500

    Added the routes for positions and trades

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'welcome#home'
+
+  resources :trades
+
+  resources :positions, only: [:index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
